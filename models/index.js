1957fd4e3dddd11b5d5d25d4b39162b7367b4d89
it-jcg <joyce.garnett@magna.com>
2022-08-26 17:38:50 -0400

    added api

--- models/index.js
+++ models/index.js
@@ -12,14 +12,6 @@ Post.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
-});
-
 Comment.belongsTo(User, {
     foreignKey: 'user_id'
 });
@@ -36,4 +28,4 @@ Comment.belongsTo(User, {
     foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
