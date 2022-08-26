51b8c83bd69ed544e674c7bc47011f8f7873ee00
it-jcg <joyce.garnett@magna.com>
2022-08-26 17:27:06 -0400

    added models: user, post, comment

--- models/index.js
+++ models/index.js
@@ -1,1 +1,39 @@
-module.exports = {};
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// Post association
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
